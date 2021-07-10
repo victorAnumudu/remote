36082121c128e0c5ec665b61707bfe62537e9622
= <=>
2021-07-10 10:29:18 +0100

    added remote server link

--- db_connection.php
+++ db_connection.php
@@ -1,10 +1,17 @@
 
 <?php 
+    // local server
+    // $localserver = "localhost";
+    // $db_user = "root";
+    // $db_password = "";
+    // $db_name = "users";
 
-    $localserver = "localhost";
-    $db_user = "root";
-    $db_password = "";
-    $db_name = "users";
+
+    // remote server
+    $localserver = "remotemysql.com";
+    $db_user = "7xH2Kt4FEk";
+    $db_password = "ry9LZeARCC";
+    $db_name = "7xH2Kt4FEk";
 
     $connection = new mysqli($localserver, $db_user, $db_password, $db_name);
 
